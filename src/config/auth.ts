9d6dcf0155bfe18a3c0da496bf67dd95764d43e9
Rickson <rickson.simoes@hotmail.com>
2021-09-20 21:49:51 -0300

    :hammer: Refatorando autenticação do usuário - refresh token

---
+++ src/config/auth.ts
@@ -0,0 +1,9 @@
+export default {
+  // md5 generator: rickson
+  secret_token: "cfbd5a5fa78bc598b883451ad7532f05",
+  expires_in_token: "15m",
+  // md5 generator: rickson2,
+  secret_refresh_token: "e443fd55a91455abaa4eb5c1ad2ee43e",
+  expires_in_refresh_token: "30d",
+  expires_refresh_token_days: 30,
+};
