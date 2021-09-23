597d26e48c044791525a5c38d7fb20e7b83b4055
Rickson <rickson.simoes@hotmail.com>
2021-09-23 08:57:13 -0300

    :wrench: :abc: typo correction

--- src/modules/accounts/useCases/refreshToken/RefreshTokenController.ts
+++ src/modules/accounts/useCases/refreshToken/RefreshTokenController.ts
@@ -7,7 +7,7 @@ class RefreshTokenController {
   async handle(request: Request, response: Response): Promise<Response> {
     const token =
       request.body.token ||
-      request.headers["x-acess-token"] ||
+      request.headers["x-access-token"] ||
       request.query.token;
 
     const refreshTokenUseCase = container.resolve(RefreshTokenUseCase);
