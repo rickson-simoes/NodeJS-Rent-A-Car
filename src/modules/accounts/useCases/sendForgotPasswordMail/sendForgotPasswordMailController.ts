43ce05511218ff8743c8c3df07a62fedccf5b884
Rickson <rickson.simoes@hotmail.com>
2021-10-02 08:18:15 -0300

    :bar_chart: Envio de e-mail - testes

--- src/modules/accounts/useCases/sendForgotPasswordMail/sendForgotPasswordMailController.ts
+++ src/modules/accounts/useCases/sendForgotPasswordMail/sendForgotPasswordMailController.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from "express";
 import { container } from "tsyringe";
 
-import { SendForgotPasswordMailUseCase } from "./sendForgotPasswordMailUseCase";
+import { SendForgotPasswordMailUseCase } from "./SendForgotPasswordMailUseCase";
 
 class SendForgotPasswordMailController {
   async handle(request: Request, response: Response): Promise<Response> {
--- src/shared/infra/http/routes/password.routes.ts
+++ src/shared/infra/http/routes/password.routes.ts
@@ -1,7 +1,7 @@
 import { Router } from "express";
 
 import { ResetPasswordUserController } from "@modules/accounts/useCases/resetPasswordUser/ResetPasswordUserController";
-import { SendForgotPasswordMailController } from "@modules/accounts/useCases/sendForgotPasswordMail/sendForgotPasswordMailController";
+import { SendForgotPasswordMailController } from "@modules/accounts/useCases/sendForgotPasswordMail/SendForgotPasswordMailController";
 
 const passwordRoutes = Router();
 
