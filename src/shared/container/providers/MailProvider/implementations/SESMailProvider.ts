b7ddedbe77e7b1dd95c7a0c0b0bf8137945e3483
Rickson <rickson.simoes@hotmail.com>
2021-10-21 16:08:41 -0300

    :hammer: :wrench: Criando provider SESMail - AWS

---
+++ src/shared/container/providers/MailProvider/implementations/SESMailProvider.ts
@@ -0,0 +1,55 @@
+import { SES } from "aws-sdk";
+import fs from "fs";
+import Handlebars from "handlebars";
+import nodemailer, { Transporter } from "nodemailer";
+import { injectable } from "tsyringe";
+
+import { IMailProvider } from "../IMailProvider";
+
+@injectable()
+class SESMailProvider implements IMailProvider {
+  private client: Transporter;
+
+  constructor() {
+    this.createClient();
+  }
+
+  private async createClient() {
+    try {
+      this.client = nodemailer.createTransport({
+        SES: new SES({
+          apiVersion: "2010-12-01",
+          region: process.env.AWS_REGION,
+        }),
+      });
+    } catch (err) {
+      console.error(`SESMailProvider - Error:\n${err}`);
+    }
+  }
+
+  async sendMail(
+    to: string,
+    subject: string,
+    variables: any,
+    path: string
+  ): Promise<void> {
+    if (!this.client) {
+      await this.createClient();
+    }
+
+    const templateFileContent = fs.readFileSync(path).toString("utf-8");
+
+    const templateParse = Handlebars.compile(templateFileContent);
+
+    const templateHTML = templateParse(variables);
+
+    await this.client.sendMail({
+      to,
+      from: "RentxApi <rickson@ricksonsimoes.com>",
+      subject,
+      html: templateHTML,
+    });
+  }
+}
+
+export { SESMailProvider };
